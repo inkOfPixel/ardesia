48426aa275ecd82d6c729998873624acc516e95c
macs <massimodemarchi91@gmail.com>
2016-05-10 00:00:40 +0200

    Working on TextField

--- source/components/TextField/index.jsx
+++ source/components/TextField/index.jsx
@@ -4,24 +4,55 @@
 */
 
 import "./style.scss";
-import React, { PropTypes } from "react";
+import React, { PropTypes, Component } from "react";
 
-const TextField = ({ value, className }) => (
-	<input
-		type="text"
-		className={ `TextField ${className}` }
-		value={value}
-	/>
-);
+class TextField extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			value: props.value,
+			onChange: props.onChange !== undefined ? props.onChange : event => this._onChange(event)
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const { value } = nextProps;
+
+		this.setState({ value });
+	}
+
+	_onChange(event) {
+		this.setState({
+			value: event.currentTarget.value
+		});
+	}
+
+	render() {
+		const { className, theme } = this.props;
+		const { value, onChange } = this.state;
+		return (
+			<input
+				type="text"
+				className={ `TextField ${theme} ${className}` }
+				value={value}
+				onChange={onChange}
+			/>
+		);
+	}
+}
 
 TextField.propTypes = {
 	value: PropTypes.string,
-	className: PropTypes.string
+	className: PropTypes.string,
+	theme: PropTypes.string,
+	onChange: PropTypes.func
 };
 
 TextField.defaultProps = {
 	value: "",
-	className: ""
+	className: "",
+	theme: "light"
 };
 
 export default TextField;

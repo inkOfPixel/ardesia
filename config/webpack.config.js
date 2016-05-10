97f9b72029c2396c0cb3405b3bc922fc80e7d2ef
macs <massimodemarchi91@gmail.com>
2016-05-10 18:24:18 +0200

    Fixing window bug

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
 	target: "node",
@@ -30,6 +31,11 @@ module.exports = {
 			}
 		]
 	},
+	node: {
+		__dirname: true,
+		__filename: true
+	},
+	externals: [nodeExternals()],
 	plugins: [
 		new webpack.NoErrorsPlugin(),
 		new webpack.DefinePlugin({

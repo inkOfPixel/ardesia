1c81e5fe375cd52d3202140a28fb65695675f570
macs <massimodemarchi91@gmail.com>
2016-05-21 18:31:48 +0200

    deploy to website

--- scripts/git-gh-deploy.sh
+++ scripts/git-gh-deploy.sh
@@ -4,4 +4,8 @@ then
   echo "Which folder do you want to deploy to GitHub Pages?"
   exit 1
 fi
+gulp build:website &&
+git add . &&
+git commit -m "deploy to $1" &&
+git push $1 master &&
 git subtree push --prefix $1 origin gh-pages

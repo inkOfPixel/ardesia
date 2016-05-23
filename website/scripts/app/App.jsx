916196710554c3479362075905ad3dd9d9584961
macs <massimodemarchi91@gmail.com>
2016-05-23 14:22:46 +0200

    Merging conflicts

--- website/scripts/app/App.jsx
+++ website/scripts/app/App.jsx
@@ -19,18 +19,12 @@ const App = ({ }) => (
 		>
 			<section>
 				<h3>TextField</h3>
-<<<<<<< HEAD
 				<hr></hr>
-=======
->>>>>>> 99aa2d373e8f0eb16614f1189e4d9071ae932a45
 				<TextField />
 			</section>
 			<section>
 				<h3>Buttons</h3>
-<<<<<<< HEAD
 				<hr></hr>
-=======
->>>>>>> 99aa2d373e8f0eb16614f1189e4d9071ae932a45
 				<StackLayout
 					spacing="5px"
 				>
@@ -122,11 +116,6 @@ const App = ({ }) => (
 							>
 								Danger
 							</Button>
-<<<<<<< HEAD
-
-=======
-							
->>>>>>> 99aa2d373e8f0eb16614f1189e4d9071ae932a45
 						</StackLayout>
 					</div>
 				</StackLayout>
